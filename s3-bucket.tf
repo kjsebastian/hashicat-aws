57a8e1b1c1fd9b11bbdef978cbe69081390e2065
Kevin <kevinjsebastian10@gmail.com>
2022-07-25 17:27:10 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,11 @@
-module "s3-bucket" {
-  source  = "app.terraform.io/kevin-tfchip/s3-bucket/aws"
-  version = "2.8.0"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
 }
